f68d98a9c8855ca27958fe82af5ec216d63fa69f
eleazarMar <72273720+eleazarMar@users.noreply.github.com>
2021-01-13 12:27:01 -0600

    Add files via upload

---
+++ test project/test project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test project
+//
+//  Created by P.M. Class on 1/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+}
---
+++ test project/test project/test_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_projectApp.swift
+//  test project
+//
+//  Created by P.M. Class on 1/13/21.
+//
+
+import SwiftUI
+
+@main
+struct test_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
